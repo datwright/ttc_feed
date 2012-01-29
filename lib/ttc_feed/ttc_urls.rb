b91129c3602c9235fd8e4471fc467a61e20ed03d
David Wright <davidwright@gmail.com>
2012-01-29 15:17:14 -0500

    Made TtcUrls an instanced class

--- lib/ttc_feed/ttc_urls.rb
+++ lib/ttc_feed/ttc_urls.rb
@@ -1,32 +1,33 @@
 require "ttc_feed/version"
 require "ttc_feed/zurl"
 
-module TtcUrls
-  def self.base
-    Zurl.new("http://webservices.nextbus.com/service/publicXMLFeed")
+class TtcUrls
+  attr_accessor :base
+
+  def initialize(agency_tag = "ttc")
+    @agency_tag = agency_tag
+    @base = Zurl.new("http://webservices.nextbus.com/service/publicXMLFeed")
   end
 
-  def self.route_list
-    base.command("routeList").a("ttc").to_s
+  def route_list
+    @base.command("routeList").a(@agency_tag).to_s
   end
 
   # route_tag = "512"
-  def self.route_config(route_tag)
-    base.command("routeConfig").a(agency_tag).r(route_tag).to_s
+  def route_config(route_tag)
+    @base.command("routeConfig").a(@agency_tag).r(route_tag).to_s
   end
 
   # stop_id = "14935"
   # route_tag = "512"
-  def self.predictions(stop_id, route_tag)
-    base.stopId(stop_id).r(route_tag).a("ttc").to_s
+  def predictions(stop_id, route_tag)
+    @base.stopId(stop_id).r(route_tag).a(@agency_tag).to_s
   end
 
   # route_tag = "512"
-  def self.vehicle_locations(route_tag, last_timestamp = nil)
-    url = base.command("vehicleLocations").a("ttc").r(route_tag)
-
+  def vehicle_locations(route_tag, last_timestamp = nil)
+    url = @base.command("vehicleLocations").a(@agency_tag).r(route_tag)
     url = url.t(last_timestamp) if last_timestamp
-
     url.to_s
   end
 
--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe "TtcUrls" do
   it "should have the proper base url set" do
-    TtcUrls.base.to_s.should == "http://webservices.nextbus.com/service/publicXMLFeed"
+    TtcUrls.new.base.to_s.should == "http://webservices.nextbus.com/service/publicXMLFeed"
   end
 end
